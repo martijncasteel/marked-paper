c033bf2e7fb36dc8814e6664f3cd62558291fe2f
Martijn Casteel <martijn.casteel@gmail.com>
2016-04-15 16:29:27 +0200

    Started adding haml as language, jade cannot dynamically include

--- app.js
+++ app.js
@@ -11,8 +11,9 @@ var api = require('./routes/api');
 var app = express();
 
 // view engine setup
+app.engine('.haml', require('hamljs').renderFile);
 app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
+app.set('view engine', 'hamljs');
 
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
@@ -39,7 +40,7 @@ app.use(function(req, res, next) {
 if (app.get('env') === 'development') {
   app.use(function(err, req, res, next) {
     res.status(err.status || 500);
-    res.render('error', {
+    res.render('error.haml', {
       message: err.message,
       error: err
     });
@@ -50,7 +51,7 @@ if (app.get('env') === 'development') {
 // no stacktraces leaked to user
 app.use(function(err, req, res, next) {
   res.status(err.status || 500);
-  res.render('error', {
+  res.render('error.haml', {
     message: err.message,
     error: {}
   });
--- routes/index.js
+++ routes/index.js
@@ -7,7 +7,7 @@ var fs = require('fs');
 router.get('/', function(req, res, next) {
   fs.readdir('./cache', function(err, items) {
 
-    res.render('index', {
+    res.render('index.haml', {
       posts: items.filter(function(item){
 
         // only html files
