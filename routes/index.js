bbf626c479f0a85944def8594f6961ae14cf63b5
Martijn Casteel <martijn.casteel@gmail.com>
2016-05-05 12:33:22 -0400

    Added infinity scrolling

--- routes/index.js
+++ routes/index.js
@@ -5,7 +5,7 @@ var fs = require('fs');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  var json = JSON.parse(process.env.posts).slice(-3, 3);
+  var json = JSON.parse(process.env.posts).slice(-2, 2);
 
   var posts = json.reverse().map(function(item){
 
@@ -18,8 +18,29 @@ router.get('/', function(req, res, next) {
   });
 });
 
-router.get(/\/([a-z\-]+)\/?$/, function(req, res, next) {
+router.get('/posts', function(req, res, next) {
+  var posts = JSON.parse(process.env.posts);
 
+  var previous = posts.filter(function(item) {
+    return item.file == req.query.previous
+  });
+
+  if( posts.indexOf(previous[0]) < 1){
+    var err = new Error('Not Found');
+    err.status = 404;
+    next(err);
+    return
+  }
+
+  var post = posts[ posts.indexOf(previous[0]) -1 ]
+
+  res.render('_post', {
+    post: post,
+    content: fs.readFileSync('./.cache/' + post.file + '.html', 'utf8')
+  });
+});
+
+router.get(/\/([a-z\-]+)\/?$/, function(req, res, next) {
   var post = JSON.parse(process.env.posts).filter(function(item) {
     return item.file == req.params[0]
   });
