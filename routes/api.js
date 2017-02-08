019a0bb005a77bbc88f2e42136c2c36f7704bd31
Martijn Casteel <martijn.casteel@gmail.com>
2017-02-08 20:22:09 +0100

    Pull changes from github when called

--- routes/api.js
+++ routes/api.js
@@ -1,8 +1,34 @@
 var express = require('express');
 var router = express.Router();
 
-router.get('/push', function(req, res, next) {
-  res.send('respond with a resource');
+var crypto = require('crypto');
+var secrets = require('../secrets');
+
+router.post('/push', function(req, res, next) {
+  var payload = JSON.stringify(req.body);
+
+  var hash = crypto.createHmac("sha1", secrets.github_token).update(payload).digest("hex");
+
+  if (req.headers['X-Hub-Signature'] === "sha1=" + hash){
+
+    var spawn = require('child_process').spawn;
+    var child = spawn('./pull.sh');
+
+    child.stdout.on('data', (data) => {
+      res.json({
+        "hub-delivery": req.headers["x-github-delivery"],
+        "hub-event": req.headers["x-github-event"],
+        "response": 'data.toString()',
+
+        "commit": req.body.after,
+        "messages": req.body.commits
+      })
+    });
+
+    return
+  }
+
+ res.status(403).json({"reponse": "forbidden"});
 });
 
 module.exports = router;
